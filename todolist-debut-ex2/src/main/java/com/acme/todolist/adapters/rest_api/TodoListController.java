b77298d152f844ea11cd35af0095e4c4aeb2db4f
Bertrand Florat <bertrand@florat.net>
2020-03-11 11:07:09 +0100

    feat: code retour addItem : 201

--- todolist-debut-ex2/src/main/java/com/acme/todolist/adapters/rest_api/TodoListController.java
+++ todolist-debut-ex2/src/main/java/com/acme/todolist/adapters/rest_api/TodoListController.java
@@ -5,9 +5,11 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.acme.todolist.application.port.in.AddTodoItem;
@@ -40,6 +42,7 @@ public class TodoListController {
 	}
 	
 	@PostMapping("/todos")
+	@ResponseStatus(code= HttpStatus.CREATED)
 	public void addTodoItem(@RequestBody TodoItem item) {
 		this.addTodoItem.addTodoItem(item);
 	}
